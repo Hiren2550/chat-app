25b7fe940e776319b70640d53f4e355828a4e88f
Hiren Shah <hirenshah2550@gmail.com>
2025-07-26 22:15:45 +0530

    theme setup: now not work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,49 +10,52 @@ import Profile from "./pages/Profile";
 import SignUp from "./pages/SignUp";
 import TermsConditions from "./pages/TermsConditions";
 import { isLogin } from "./redux/auth/authSlice";
+import { ThemeProvider } from "./contextApi/themeProvider";
 
 function App() {
   const isAuthenticated = useSelector(isLogin);
   return (
-    <BrowserRouter>
-      <Toaster />
-      <Routes>
-        <Route
-          path="/signup"
-          element={isAuthenticated ? <Navigate to={"/"} /> : <SignUp />}
-        />
-        <Route
-          path="/login"
-          element={isAuthenticated ? <Navigate to={"/"} /> : <Login />}
-        />
-        <Route path="/terms-privacy" element={<TermsConditions />} />
-        <Route
-          path="/"
-          element={
-            <PrivateRoute>
-              <Home />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/profile"
-          element={
-            <PrivateRoute>
-              <Profile />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <PrivateRoute>
-              <About />
-            </PrivateRoute>
-          }
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <BrowserRouter>
+        <Toaster />
+        <Routes>
+          <Route
+            path="/signup"
+            element={isAuthenticated ? <Navigate to={"/"} /> : <SignUp />}
+          />
+          <Route
+            path="/login"
+            element={isAuthenticated ? <Navigate to={"/"} /> : <Login />}
+          />
+          <Route path="/terms-privacy" element={<TermsConditions />} />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/profile"
+            element={
+              <PrivateRoute>
+                <Profile />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <PrivateRoute>
+                <About />
+              </PrivateRoute>
+            }
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
