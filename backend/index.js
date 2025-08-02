6ca04e1380351026f03f999cc649fa9f03d87b8b
Hiren Shah <hirenshah2550@gmail.com>
2025-08-03 01:13:28 +0530

    deployement setup

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,18 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import errorHandler from "./utils/errorHandler.js";
 import { app, server } from "./lib/socket.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
-app.use(express.static("public"));
 app.use(cookieParser());
 app.use(
   cors({
@@ -21,7 +25,7 @@ app.use(
     credentials: true, // If you use cookies/auth
   })
 );
-
+app.use(express.static(path.join(__dirname, "frontend/dist")));
 // === Log each incoming request ===
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
@@ -30,10 +34,16 @@ app.use((req, res, next) => {
 
 // API endpoints
 app.use("/api", routes);
+// ✅ Serve static files from frontend/dist
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+// ✅ Catch-all route to serve React app (for client-side routing)
 
 // Error handling middleware
 app.use(errorHandler);
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 server.listen(port, async () => {
   await connectDB();
   console.log(`Server is running on port ${port}... ��`);
